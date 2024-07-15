2a9887f7e82cfc639043cd0e978aed31bbd1b297
Garegin Yervandyan <garegin.y.g@gmail.com>
2024-07-15 20:07:30 +0400

    Implemented UniswapV3Pool contract with initial setup and constants
    - Added constants  and  for tick range constraints.
    - Defined  struct to hold current price and tick information.
    - Initialized , , , and  variables in constructor for pool setup.
    - Established mappings  and  for managing tick and position information.

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -8,4 +8,35 @@ contract UniswapV3Pool {
     using Tick for mapping (int24 => Tick.Info);
     using Position for mapping (bytes32 => Position.Info);
     using Position for Position.Info;
+
+   int24 internal constant MIN_TICK = -887272;
+    int24 internal constant MAX_TICK = -MIN_TICK;
+
+//Pool tokens
+    address public immutable token0;
+    address public immutable token1;
+
+//The Slot0 struct holds the current price and tick information for the pool.
+    struct Slot0 {
+        uint160 sqrtPriceX96;
+        int24 tick;
+    }
+
+    Slot0 public slot0;
+
+//amount of all liquidity: L
+    uint128 public liquidity;
+
+// Ticks info
+    mapping(int24 => Tick.Info) public ticks;
+// Position Info
+    mapping(bytes32 => Position.Info) public positions;
+
+    constructor(address _token0,address _token1,uint160 _sqrtPriceX96,int24 _tick) {
+        token0 = _token0;
+        token1 = _token1;
+        slot0 = Slot0({sqrtPriceX96: _sqrtPriceX96,tick: _tick});
+    }
+
+
 }
