e54eec7a4cbdbd9657231b73c0add9739cee5406
Garegin Yervandyan <garegin.y.g@gmail.com>
2024-07-17 00:03:40 +0400

    adding new function in lib but not added functionality

--- src/lib/Tick.sol
+++ src/lib/Tick.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.26;
 
-contract Tick {
-    
+library Tick {
+    function tickSpacingToMaxLiquidityPerTick(int24 _tickSpacing)  pure returns (uint128) {
+        //To do write code
+    }
 }
