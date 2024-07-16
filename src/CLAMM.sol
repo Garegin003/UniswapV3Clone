923d5af6e5e8c1108d331e14b03c8b124b871f5e
Garegin Yervandyan <garegin.y.g@gmail.com>
2024-07-17 00:05:14 +0400

    importing Tick.sol and adding new varibale maxLiquidityPerTick

--- src/CLAMM.sol
+++ src/CLAMM.sol
@@ -1,19 +1,22 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.26;
 
+import "./lib/Tick.sol";
+
 contract CLAMM {
 
     address immutable token0;
     address immutable token1;
     uint24 immutable fee;
-    int24 immutable tickspacing;
+    int24 immutable tickSpacing;
+    uint128 public maxLiquidityPerTick;
 
-
-    constructor(address _token0,address _token1,uint24 _fee,int24 _tickspacing) {
+    constructor(address _token0,address _token1,uint24 _fee,int24 _tickSpacing) {
         token0 = _token0;
         token1 = _token1;
         fee = _fee;
-        tickspacing = _tickspacing;
+        tickSpacing = _tickSpacing;
+        maxLiquidityPerTick = Tick.tickSpacingToMaxLiquidityPerTick(_tickSpacing);
     }
 }
 
