b78e809e40da802669f2ab385aeb23615adecc5a
Garegin Yervandyan <garegin.y.g@gmail.com>
2024-07-18 15:57:30 +0400

    doing tickSpacingToMaxLiquidityPerTick function to internal

--- src/lib/Tick.sol
+++ src/lib/Tick.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.26;
 
 library Tick {
-    function tickSpacingToMaxLiquidityPerTick(int24 _tickSpacing)  pure returns (uint128) {
+    function tickSpacingToMaxLiquidityPerTick(int24 _tickSpacing) internal  pure returns (uint128) {
         //To do write code
     }
 }
